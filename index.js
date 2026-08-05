2b0e706a1f119416132424ed17fe2ef54f30db07
Joao <146687118+Joao-ArthurDS@users.noreply.github.com>
2026-08-05 19:37:43 -0300

    fully functional player turn checking and marking the board

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const ticTacToe = (() => {
     let board;
+    let turn;
+    let player1;
+    let player2;
+    let validMove;
 
-    const newGame = () => {
+    const newGame = (p1,p2) => {
+        player1 = createPlayer(p1,1);
+        player2 = createPlayer(p2,2);
+        turn = player1;
         board = [[0,0,0],[0,0,0],[0,0,0]];
         return board;
     };
 
-    const winner = (player) => {
+    const checkWin = (player) => {
+        
+        player.score++;
+        const win = player.score === 3 ? true : false;
         return console.log(player.name);
     };
 
@@ -15,12 +25,47 @@ const ticTacToe = (() => {
     }
 
     const updateBoard = (player,x,y) => {
-        board[x][y] = player.marker;
+        if(board[x][y] === 0){
+            board[x][y] = player.marker;
+            validMove = true;
+        }else{
+            board[x][y] = board[x][y];
+            validMove = false;
+        }
         return board;
     }
 
+    const setTurn = () => {
+        if(validMove) {
+            if (turn === player1){
+                return turn = player2;
+            }else{
+                return turn = player1;
+            }
+        }else{
+            return turn;
+        }
+    }
+
+    const getTurn = () => {
+        return turn;
+    }
+
+    return { newGame, winner, getBoard, updateBoard, setTurn, getTurn }
 })();
 
 function createPlayer(name, marker){
-    return { name, marker };
+    const score = 0;
+    return { name, marker, score };
 };
+
+function startGame(player1name, player2name){
+    ticTacToe.newGame(player1name,player2name);
+};
+
+function makePlay(position){
+    const player = ticTacToe.getTurn();
+    ticTacToe.updateBoard(player,position[0],position[1]);
+    ticTacToe.setTurn();
+};
+
